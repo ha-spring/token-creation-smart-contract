9fdaa39caa77194fc22fa3999311686426facb2a
ha-spring <spring4boot@gmail.com>
2023-07-19 09:11:08 +0000

    add ERC20TokenFactory

--- contracts/ERC20TokenFactory.sol
+++ contracts/ERC20TokenFactory.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20TokenFactory  is Ownable {
-    event TokenCreated(address indexed tokenAddress, string name, string symbol);
+contract ERC20TokenFactory is Ownable {
+    event TokenCreated(
+        address indexed tokenAddress,
+        string name,
+        string symbol
+    );
 
     uint256 private _creationFee;
 
@@ -26,27 +29,29 @@ contract ERC20TokenFactory  is Ownable {
     ) external payable returns (address) {
         require(msg.value >= _creationFee, "Insufficient fee");
 
-        ERC20Token token;
+        ERC20 token;
 
         if (isMintable && isBurnable && isPausable) {
-            token = new MintableBurnablePausableERC20Token(name, symbol);
+            token = new MintableBurnablePausableERC20Token(
+                name,
+                symbol,
+                initialSupply
+            );
         } else if (isMintable && isBurnable) {
-            token = new MintableBurnableERC20Token(name, symbol);
+            token = new MintableBurnableERC20Token(name, symbol, initialSupply);
         } else if (isMintable && isPausable) {
-            token = new MintablePausableERC20Token(name, symbol);
+            token = new MintablePausableERC20Token(name, symbol, initialSupply);
         } else if (isBurnable && isPausable) {
-            token = new BurnablePausableERC20Token(name, symbol);
+            token = new BurnablePausableERC20Token(name, symbol, initialSupply);
         } else if (isMintable) {
-            token = new MintableERC20Token(name, symbol);
+            token = new MintableERC20Token(name, symbol, initialSupply);
         } else if (isBurnable) {
-            token = new BurnableERC20Token(name, symbol);
+            token = new BurnableERC20Token(name, symbol, initialSupply);
         } else if (isPausable) {
-            token = new PausableERC20Token(name, symbol);
+            token = new PausableERC20Token(name, symbol, initialSupply);
         } else {
-            token = new ERC20Token(name, symbol);
+            token = new ERC20Token(name, symbol, initialSupply);
         }
-
-        token._mint(msg.sender, initialSupply);
 
         emit TokenCreated(address(token), name, symbol);
 
@@ -66,51 +71,160 @@ contract ERC20TokenFactory  is Ownable {
     }
 }
 
-contract ERC20Token is ERC20, ERC20Permit {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {}
+contract ERC20Token is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
+        if (initialSupply > 0) {
+            _mint(msg.sender, initialSupply);
+        }
+    }
 }
 
-contract MintableERC20Token is ERC20Token {
-    constructor(string memory name, string memory symbol) ERC20Token(name, symbol) {}
+contract MintableERC20Token is ERC20Token, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20Token(name, symbol, initialSupply) {}
 
-    function mint(address account, uint256 amount) external {
+    function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 }
 
-contract BurnableERC20Token is ERC20Token {
-    constructor(string memory name, string memory symbol) ERC20Token(name, symbol) {}
-
-    function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
-    }
+contract BurnableERC20Token is ERC20Token, ERC20Burnable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20Token(name, symbol, initialSupply) {}
 }
 
-contract PausableERC20Token is ERC20Token {
-    constructor(string memory name, string memory symbol) ERC20Token(name, symbol) {}
+contract PausableERC20Token is ERC20Token, Pausable, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20Token(name, symbol, initialSupply) {}
 
-    function pause() external {
+    function pause() public onlyOwner {
         _pause();
     }
 
-    function unpause() external {
+    function unpause() public onlyOwner {
         _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
 
-contract MintableBurnableERC20Token is MintableERC20Token, BurnableERC20Token {
-    constructor(string memory name, string memory symbol) MintableERC20Token(name, symbol) BurnableERC20Token(name, symbol) {}
+contract MintableBurnableERC20Token is ERC20Token, ERC20Burnable, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20Token(name, symbol, initialSupply) {}
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
 }
 
-contract MintablePausableERC20Token is MintableERC20Token, PausableERC20Token {
-    constructor(string memory name, string memory symbol) MintableERC20Token(name, symbol) PausableERC20Token(name, symbol) {}
+contract MintablePausableERC20Token is ERC20Token, Pausable, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20Token(name, symbol, initialSupply) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
 
-contract BurnablePausableERC20Token is BurnableERC20Token, PausableERC20Token {
-    constructor(string memory name, string memory symbol) BurnableERC20Token(name, symbol) PausableERC20Token(name, symbol) {}
+contract BurnablePausableERC20Token is
+    ERC20Token,
+    ERC20Burnable,
+    Pausable,
+    Ownable
+{
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20Token(name, symbol, initialSupply) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
 
-contract MintableBurnablePausableERC20Token is MintableERC20Token, BurnableERC20Token, PausableERC20Token {
-    constructor(string memory name, string memory symbol) MintableERC20Token(name, symbol) BurnableERC20Token(name, symbol) PausableERC20Token(name, symbol) {}
-}
+contract MintableBurnablePausableERC20Token is
+    ERC20Token,
+    ERC20Burnable,
+    Pausable,
+    Ownable
+{
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20Token(name, symbol, initialSupply) {}
 
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200, // Adjust the number of runs to optimize gas cost
+      },
+    },
+  },
 };
 
 export default config;
