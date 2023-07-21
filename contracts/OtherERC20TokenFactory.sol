4b778913e2fcb44479f5a60b424f3cf1d7f7780a
ha-spring <spring4boot@gmail.com>
2023-07-21 10:51:07 +0000

    add OtherERC20TokenFactory and tests

---
+++ contracts/OtherERC20TokenFactory.sol
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ERC20Token.sol";
+
+library OtherERC20TokenLibrary {
+    function createToken(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address msgSender
+    ) internal returns (ERC20Token) {
+        return new ERC20Token(name, symbol, initialSupply, msgSender);
+    }
+
+    function createBurnableToken(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address msgSender
+    ) internal returns (BurnableERC20Token) {
+        return new BurnableERC20Token(name, symbol, initialSupply, msgSender);
+    }
+
+    function createPausableToken(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address msgSender
+    ) internal returns (PausableERC20Token) {
+        return new PausableERC20Token(name, symbol, initialSupply, msgSender);
+    }
+
+    function createBurnablePausableToken(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address msgSender
+    ) internal returns (BurnablePausableERC20Token) {
+        return
+            new BurnablePausableERC20Token(
+                name,
+                symbol,
+                initialSupply,
+                msgSender
+            );
+    }
+}
+
+contract OtherERC20TokenFactory is Ownable {
+    using OtherERC20TokenLibrary for *;
+
+    event TokenCreated(
+        address indexed tokenAddress,
+        string name,
+        string symbol
+    );
+
+    uint256 private _creationFee;
+    address private _feesReceiver;
+
+    constructor(uint256 creationFee, address feesReceiver) {
+        require(feesReceiver != address(0), "Invalid receiver address");
+        _creationFee = creationFee;
+        _feesReceiver = feesReceiver;
+    }
+
+    function createERC20Token(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        bool isBurnable,
+        bool isPausable
+    ) external payable returns (address) {
+        require(msg.value >= _creationFee, "Insufficient fee");
+
+        ERC20Token token;
+
+        if (isBurnable && isPausable) {
+            token = OtherERC20TokenLibrary.createBurnablePausableToken(
+                name,
+                symbol,
+                initialSupply,
+                msg.sender
+            );
+        } else if (isBurnable) {
+            token = OtherERC20TokenLibrary.createBurnableToken(
+                name,
+                symbol,
+                initialSupply,
+                msg.sender
+            );
+        } else if (isPausable) {
+            token = OtherERC20TokenLibrary.createPausableToken(
+                name,
+                symbol,
+                initialSupply,
+                msg.sender
+            );
+        } else {
+            token = OtherERC20TokenLibrary.createToken(
+                name,
+                symbol,
+                initialSupply,
+                msg.sender
+            );
+        }
+
+        payable(_feesReceiver).transfer(_creationFee);
+
+        emit TokenCreated(address(token), name, symbol);
+
+        if (msg.value > _creationFee) {
+            payable(msg.sender).transfer(msg.value - _creationFee);
+        }
+
+        return address(token);
+    }
+
+    function setCreationFee(uint256 fee) external onlyOwner {
+        _creationFee = fee;
+    }
+
+    function getCreationFee() external view returns (uint256) {
+        return _creationFee;
+    }
+
+    function setFeesReceiver(address receiver) external onlyOwner {
+        require(receiver != address(0), "Invalid receiver address");
+        _feesReceiver = receiver;
+    }
+
+    function getFeesReceiver() external view returns (address) {
+        return _feesReceiver;
+    }
+}
+
+contract BurnableERC20Token is ERC20Token, ERC20Burnable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address msgSender
+    ) ERC20Token(name, symbol, initialSupply, msgSender) {}
+}
+
+contract PausableERC20Token is ERC20Token, Pausable, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address msgSender
+    ) ERC20Token(name, symbol, initialSupply, msgSender) {
+        _transferOwnership(msgSender);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
+
+contract BurnablePausableERC20Token is
+    ERC20Token,
+    ERC20Burnable,
+    Pausable,
+    Ownable
+{
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address msgSender
+    ) ERC20Token(name, symbol, initialSupply, msgSender) {
+        _transferOwnership(msgSender); 
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
---
+++ test/other.ts
@@ -0,0 +1,198 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("OtherERC20TokenFactory", function () {
+  let OtherERC20TokenFactory;
+  let factory;
+  let owner;
+  let addr1;
+  let addr2;
+  let addr3;
+  let feesReceiver;
+  let newFeesReceiver;
+  let ERC20Token;
+  let BurnableERC20Token;
+  let PausableERC20Token;
+  let BurnablePausableERC20Token;
+  let fees = ethers.parseEther("0.01");
+
+  beforeEach(async function () {
+    [owner, addr1, addr2, addr3, feesReceiver, newFeesReceiver] = await ethers.getSigners();
+
+    OtherERC20TokenFactory = await ethers.getContractFactory("OtherERC20TokenFactory");
+    factory = await OtherERC20TokenFactory.deploy(fees, feesReceiver);
+
+    ERC20Token = await ethers.getContractFactory("ERC20Token");
+    BurnableERC20Token = await ethers.getContractFactory("BurnableERC20Token");
+    PausableERC20Token = await ethers.getContractFactory("PausableERC20Token");
+    BurnablePausableERC20Token = await ethers.getContractFactory("BurnablePausableERC20Token");
+  });
+
+  it("Should create a ERC20 token", async function () {
+    const name = "MyToken";
+    const symbol = "MT";
+    const initialSupply = ethers.parseEther("2000");
+    
+    const tx = await factory.createERC20Token(
+      name,
+      symbol,
+      initialSupply,
+      false,
+      false,
+      { value: fees },
+    );
+    const receipt = await tx.wait();
+    const TokenCreatedLog = receipt.logs.filter(log => log?.fragment?.name === "TokenCreated")
+    const tokenAddress = TokenCreatedLog[0].args[0]
+    const token = await ERC20Token.attach(tokenAddress); 
+    const ownerBalance = await token.balanceOf(owner.address);
+
+    expect(await token.name()).to.equal(name);
+    expect(await token.symbol()).to.equal(symbol);
+    expect(await ownerBalance).to.equal(ethers.parseEther("2000"));
+    expect(await ethers.provider.getBalance(feesReceiver.address)).to.equal("10000010000000000000000")
+  });
+
+  it("Should create a burnable ERC20 token", async function () {
+    const name = "MyBurnableToken";
+    const symbol = "MBT";
+    const initialSupply = ethers.parseEther("5000");
+
+    const tx = await factory.createERC20Token(
+      name,
+      symbol,
+      initialSupply,
+      true,
+      false,
+      { value: fees },
+    );
+    const receipt = await tx.wait();
+    const TokenCreatedLog = receipt.logs.filter(log => log?.fragment?.name === "TokenCreated")
+    const tokenAddress = TokenCreatedLog[0].args[0]
+    const token = await BurnableERC20Token.attach(tokenAddress);
+    const ownerBalance = await token.balanceOf(owner.address);
+
+    expect(await token.name()).to.equal(name);
+    expect(await token.symbol()).to.equal(symbol);
+    expect(await ownerBalance).to.equal(ethers.parseEther("5000"));
+
+    await token.burn(ethers.parseEther("3000"))
+    const ownerBalanceAfterBurn = await token.balanceOf(owner.address);
+    expect(await ownerBalanceAfterBurn).to.equal(ethers.parseEther("2000"));
+    expect(await ethers.provider.getBalance(feesReceiver.address)).to.equal("10000020000000000000000")
+  });
+
+  it("Should create a pausable ERC20 token", async function () {
+    const name = "MyPausableToken";
+    const symbol = "MPT";
+    const initialSupply = ethers.parseEther("6000");
+
+    const tx = await factory.connect(addr1).createERC20Token(
+      name,
+      symbol,
+      initialSupply,
+      false,
+      true,
+      { value: fees },
+    );
+    const receipt = await tx.wait();
+    const TokenCreatedLog = receipt.logs.filter(log => log?.fragment?.name === "TokenCreated")
+    const tokenAddress = TokenCreatedLog[0].args[0]
+    const token = await PausableERC20Token.attach(tokenAddress);
+    const ownerBalance = await token.balanceOf(addr1.address);
+    
+    expect(await token.name()).to.equal(name);
+    expect(await token.symbol()).to.equal(symbol);
+    expect(await ownerBalance).to.equal(ethers.parseEther("6000"));
+    
+    await token.connect(addr1).pause()
+    await expect(token.connect(addr1).transfer(owner.address, ethers.parseEther("1000"))).to.be.revertedWith("Pausable: paused");
+    await token.connect(addr1).unpause()
+    await token.connect(addr1).transfer(owner.address, ethers.parseEther("1200"))
+    const ownerBalanceAfterPause = await token.balanceOf(addr1.address);
+    expect(await ownerBalanceAfterPause).to.equal(ethers.parseEther("4800"));
+    expect(await ethers.provider.getBalance(feesReceiver.address)).to.equal("10000030000000000000000")
+  });
+
+  it("Should create a burnable and pausable ERC20 token", async function () {
+    const name = "MyAllFeaturesToken";
+    const symbol = "MAT";
+    const initialSupply = ethers.parseEther("8000");
+
+    const tx = await factory.createERC20Token(
+      name,
+      symbol,
+      initialSupply,
+      true,
+      true,
+      { value: fees },
+    );
+    const receipt = await tx.wait();
+    const TokenCreatedLog = receipt.logs.filter(log => log?.fragment?.name === "TokenCreated")
+    const tokenAddress = TokenCreatedLog[0].args[0]
+    const token = await BurnablePausableERC20Token.attach(tokenAddress);
+    const ownerBalance = await token.balanceOf(owner.address);
+
+    expect(await token.name()).to.equal(name);
+    expect(await token.symbol()).to.equal(symbol);
+    expect(await ownerBalance).to.equal(ethers.parseEther("8000"));
+    
+    await token.pause()
+    await expect(token.burn( ethers.parseEther("1000"))).to.be.revertedWith("Pausable: paused");
+    await token.unpause()
+    
+    await token.burn(ethers.parseEther("3000"))
+    const ownerBalanceAfterBurn = await token.balanceOf(owner.address);
+    expect(await ownerBalanceAfterBurn).to.equal(ethers.parseEther("5000"));
+    expect(await ethers.provider.getBalance(feesReceiver.address)).to.equal("10000040000000000000000")
+  });
+
+  it("Should revert if creation fee is not sufficient", async function () {
+    const name = "InsufficientFeeToken";
+    const symbol = "IFT";
+    const initialSupply = ethers.parseEther("100");
+
+    // Set a higher creation fee to intentionally make it insufficient
+    await factory.setCreationFee(ethers.parseEther("1"));
+
+    await expect(
+      factory.createERC20Token(
+        name,
+        symbol,
+        initialSupply,
+        false,
+        false,
+      ),
+    ).to.be.revertedWith("Insufficient fee");
+  });
+
+  it("Should update the creation fee", async function () {
+    const newCreationFee = ethers.parseEther("0.1");
+    await factory.setCreationFee(newCreationFee);
+    expect(await factory.getCreationFee()).to.equal(newCreationFee);
+    await factory.createERC20Token(
+      "Test",
+      "TEST",
+      0,
+      true,
+      true,
+      { value: newCreationFee },
+    ); 
+    expect(await ethers.provider.getBalance(feesReceiver.address)).to.equal("10000140000000000000000")
+  });
+  
+  it("Should update fees receiver", async function () {
+    const newCreationFee = ethers.parseEther("0.1");
+    await factory.setFeesReceiver(newFeesReceiver);
+    await factory.createERC20Token(
+      "Test",
+      "TEST",
+      0,
+      true,
+      true,
+      { value: newCreationFee },
+    ); 
+    expect(await ethers.provider.getBalance(newFeesReceiver.address)).to.equal("10000010000000000000000")
+    expect(await ethers.provider.getBalance(feesReceiver.address)).to.equal("10000140000000000000000")
+  });
+});
