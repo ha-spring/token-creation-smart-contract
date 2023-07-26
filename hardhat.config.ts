9f04df08d9acca49b62e36666fa402edd4b66849
ha-spring <spring4boot@gmail.com>
2023-07-26 09:01:51 +0000

    fix network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,17 +4,17 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.19",
-    networks: {
-      mumbai: {
-        url: "https://polygon-mumbai.g.alchemy.com/v2/0xz4yVzoZks5G-zvHN_fPW06x6ai7ayL",
-        accounts: [""],
-      },
-    },
     settings: {
       optimizer: {
         enabled: true,
         runs: 200, // Adjust the number of runs to optimize gas cost
       },
+    },
+  },
+  networks: {
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/0xz4yVzoZks5G-zvHN_fPW06x6ai7ayL",
+      accounts: [""],
     },
   },
 };
